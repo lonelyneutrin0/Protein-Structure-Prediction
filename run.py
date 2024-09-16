559d10abed9cfe9e04695092e4325ac8142e5e81
Pixelqted <pixelatedwastaken@gmail.com>
2024-09-16 11:20:14 -0400

    Updated docstrings and separated run_data

--- run.py
+++ run.py
@@ -15,9 +15,11 @@ from src import get_energy, get_conformation
 
 def example(callback):
     
-    length = 21
+    length = 13
     num_steps = 2749
-    res = np.random.choice([0,1], (length))
+    res = np.array([0,0,0,0,0,1,1,1,1,1])
+    print(res.size)
+
     # define annealing arguments, track time to run
     kwargs = {
         'residues': res, 
@@ -34,52 +36,54 @@ def example(callback):
     sec_elapsed = present - past
     print(f'Annealer run with {num_steps:.0f} steps took {sec_elapsed:.3f} seconds')
     print(f'Lowest Energy: {run.optimalest_energy} \n Optimal Energy: {run.optimal_energies[-1]}')
-    # if callback specified, results are already printed by callback
-    if callback:
-        return
-
-    # if callback not specified, plot results
+ 
+    # Run Data
     plt.plot(run.param_inv_temps, run.optimal_energies)
     plt.axhline(run.optimalest_energy, color='black', linestyle=':')
     plt.grid()
-    plt.xlabel(r'$\beta$')
+    plt.xlabel(r'$\frac{1}{T}$')
     plt.ylabel('energy')
-    plt.savefig('annealing.png', dpi=800, bbox_inches='tight')
+    plt.savefig('run_data/annealing.png', dpi=800, bbox_inches='tight')
     plt.close()
     
-    # plot num accepts and num rejects
-    plt.plot(np.arange(num_steps) / 1e+3, run.param_num_rejects / 1e+3)
+    # Acceptance vs Progress of Annealing
+    plt.plot(np.arange(num_steps), run.param_num_rejects)
     plt.grid()
-    plt.xlabel('metropolis step / $10^3$')
-    plt.ylabel('number of acceptances / $10^3$')
-    plt.savefig('num_accepts.png', dpi=800, bbox_inches='tight')
+    plt.xlabel('metropolis step')
+    plt.ylabel('number of acceptances')
+    plt.savefig('run_data/num_accepts.png', dpi=800, bbox_inches='tight')
     plt.close()
     
+    # 2D Projection of Model
     optimal_conformation = run.optimalest_conformation
+    plt.figure(figsize=(8, 6))
+    plt.scatter(optimal_conformation[:, 0], optimal_conformation[:, 1], color='black', marker='o', s=50)
+    plt.plot(optimal_conformation[:, 0], optimal_conformation[:, 1], marker='o', color='black', linestyle='-')
+    plt.title('2D Projection of the Protein Model')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.savefig('run_data/projection.png', dpi=800, bbox_inches='tight')
+    plt.close()
+    
     if optimal_conformation.shape[1] != 3:
         raise ValueError("The optimal conformation must have 3 columns representing x, y, z coordinates.")
     
-    # Plotting the optimal conformation
+    # Optimal Configuration
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-
-    # Plot each residue as a point
     ax.scatter(optimal_conformation[:, 0], optimal_conformation[:, 1], optimal_conformation[:, 2], c='k', marker='o',s=100)
-    
-    # Optionally, connect the residues with lines to visualize the backbone
     for i in range(len(optimal_conformation) - 1):
         ax.plot([optimal_conformation[i, 0], optimal_conformation[i + 1, 0]],
                 [optimal_conformation[i, 1], optimal_conformation[i + 1, 1]],
                 [optimal_conformation[i, 2], optimal_conformation[i + 1, 2]], c='k', lw=1)
     
-    # Add labels and title
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title('Optimal Protein Conformation')
-    
-    # Show the plot
-    plt.savefig('conformation.png', dpi=800, bbox_inches='tight')
+
+    plt.savefig('run_data/conformation.png', dpi=800, bbox_inches='tight')
     plt.show()
     plt.close()
 
